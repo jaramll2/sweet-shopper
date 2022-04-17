36588b815fa8a913e49820d71c7152d7194ccbeb
Miyuki Nozawa <nozawa.myk@gmail.com>
2022-04-17 17:18:31 -0400

    update routes with previous login logic

--- client/Routes.js
+++ client/Routes.js
@@ -1,65 +1,69 @@
-import React, {Component, Fragment} from 'react'
-import {connect} from 'react-redux'
-import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
-import { Login, Signup } from './components/AuthForm/AuthForm';
-import Home from './components/Home';
-import CandyList from './components/CandyList';
-import Candy from './components/Candy';
-import {me} from './store';
-import Cart from './components/Cart';
-import { guestCart } from './store/guestCart';
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Login, Signup } from "./components/AuthForm/AuthForm";
+import Home from "./components/Home";
+import CandyList from "./components/CandyList";
+import Candy from "./components/Candy";
+import { me } from "./store";
+import Cart from "./components/Cart";
+import { guestCart } from "./store/guestCart";
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
   async componentDidMount() {
-    this.props.loadInitialData()
-    
-    if(!this.props.isLoggedIn){
+    this.props.loadInitialData();
+
+    if (!this.props.isLoggedIn) {
       this.props.loadGuestCart();
     }
   }
 
   render() {
-    const {isLoggedIn} = this.props
+    const { isLoggedIn } = this.props;
 
     return (
       <div>
         <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path='/candy/:id' component={Candy}/>
-        <Route path='/candy' component={CandyList}/>
-        <Route path='/cart' component={Cart}/>
+        {!isLoggedIn && (
+          <>
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </>
+        )}
+        <Route path="/candy/:id" component={Candy} />
+        <Route path="/candy" component={CandyList} />
+        <Route path="/cart" component={Cart} />
+        <Redirect to="/" />
       </div>
-    )
+    );
   }
-
 }
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id
-  }
-}
+    isLoggedIn: !!state.auth.id,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     loadInitialData() {
-      dispatch(me())
+      dispatch(me());
     },
     loadGuestCart() {
-      dispatch(guestCart())
-    }
-  }
-}
+      dispatch(guestCart());
+    },
+  };
+};
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
+export default withRouter(connect(mapState, mapDispatch)(Routes));
