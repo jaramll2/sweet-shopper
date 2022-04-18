2214f0f22b2cd0ff22592dc0340178acaeffc7e2
jaramll2 <mjaramillo_2014@yahoo.com>
2022-04-17 20:57:09 -0500

    refractored add to cart

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,32 +1,27 @@
 import axios from 'axios';
 
-
 const ADD_TO_CART = 'ADD_TO_CART';
 const ADD_TO_CART_GUEST = 'ADD_TO_CART_GUEST';
 
-
 export const addToCart = (candy,auth, guestCart)=>{
   return async(dispatch)=>{
+    const cart = (!window.localStorage.token && guestCart) ? guestCart : auth.cart;
+
+    let line = (!cart.lineitems) ? null : cart.lineitems.find(_line => {
+      return _line.candyId === candy.id;
+    });
+
+    //if no line item exist create one to add to cart
+    if(!line){
+      (await axios.post('/api/lineItem',{cartId:cart.id, qty: 1, candyId: candy.id})).data; 
+    }
+    else{
+      //else update line item in cart 
+      (await axios.put(`/api/lineItem/${line.id}`, {...line, ...{qty: line.qty + 1}}));
+    }
+
     if(!window.localStorage.token && guestCart){
-      let line;
-
-      //if cart is not empty and has lineItems find line with candyId and update
-      if(guestCart.lineitems){
-        line = guestCart.lineitems.find(_line => {
-          return _line.candyId === candy.id;
-        });
-      }
-
-      //if no line item exist create one to add to cart
-      if(!line){
-        (await axios.post('/api/lineItem',{cartId:guestCart.id, qty: 1, candyId: candy.id})).data; 
-      }
-      else{
-        //else update line item in cart 
-        (await axios.put(`/api/lineItem/${line.id}`, {...line, ...{qty: line.qty + 1}}));
-      }
-
-      const updatedcart = (await axios.get(`/api/cart/${guestCart.id}`)).data ;
+      const updatedcart = (await axios.get(`/api/cart/${cart.id}`)).data ;
       guestCart.lineitems = updatedcart.lineitems;
 
       return dispatch({
@@ -35,24 +30,7 @@ export const addToCart = (candy,auth, guestCart)=>{
       })
     }
     else{
-      const cart = auth.cart;
-
-      //find line that has the candyId
-      const line = cart.lineitems.find(_line => {
-        return _line.candyId === candy.id;
-      });
-
-      //if no line item exist create one to add to cart
-      if(!line){
-        (await axios.post('/api/lineItem',{cartId:cart.id, qty: 1, candyId: candy.id})).data; 
-      }
-      else{
-        //else update line item in cart 
-        (await axios.put(`/api/lineItem/${line.id}`, {...line, ...{qty: line.qty + 1}}));
-      }
-
       const updatedcart = (await axios.get('/api/cart/',{headers: {authorization: window.localStorage.token}})).data ;
-
       auth.cart = updatedcart;
 
       return dispatch({
