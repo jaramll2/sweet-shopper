ce1639233bb6de40be1b3668dd57a4a8503548e4
Miyuki Nozawa <nozawa.myk@gmail.com>
2022-04-20 23:33:12 -0400

    refactor and add styling

--- client/components/Navbar/Navbar.js
+++ client/components/Navbar/Navbar.js
@@ -2,28 +2,19 @@ import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 
-import PersonIcon from "@mui/icons-material/Person";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import SearchIcon from "@mui/icons-material/Search";
-import MenuIcon from "@mui/icons-material/Menu";
-import InputAdornment from "@mui/material/InputAdornment";
-import TextField from "@mui/material/TextField";
-import Modal from "@mui/material/Modal";
-import Box from "@mui/material/Box";
 
 import { logout } from "../../store/auth";
+import NavbarSearch from "../NavbarSearch";
+import MobileNavbar from "../MobileNavbar";
+import AccountMenu from "../AccountMenu";
 
 import "./Navbar.scss";
 
 class Navbar extends Component {
   state = {
-    searchFocused: false,
-    searchText: "",
     navbarScrolled: false,
     isHomePage: true,
-    mobileNavOpen: false,
-    userMenuOpen: false,
-    loginModalOpen: false,
   };
 
   componentDidMount() {
@@ -37,25 +28,6 @@ class Navbar extends Component {
     window.removeEventListener("scroll", this.handleScroll);
   }
 
-  toggleSearch = () => {
-    this.setState(
-      (prev) => ({ searchFocused: !prev.searchFocused }),
-      () => {
-        if (this.state.searchFocused) {
-          this.searchInput.focus();
-        }
-      }
-    );
-  };
-
-  handleFocusOut = () => {
-    this.setState({ searchFocused: false, searchText: "" });
-  };
-
-  handleSearchChange = (event) => {
-    this.setState({ searchText: event.target.value });
-  };
-
   handleScroll = () => {
     const verticalPosition = window.scrollY;
     if (verticalPosition > 80) {
@@ -65,68 +37,15 @@ class Navbar extends Component {
     }
   };
 
-  toggleMobileNav = () => {
-    this.setState((prev) => ({ mobileNavOpen: !prev.mobileNavOpen }));
-  };
-
-  toggleUserMenu = () => {
-    this.setState((prev) => ({ userMenuOpen: !prev.userMenuOpen }));
-  };
-
-  toggleLoginModal = () => {
-    this.setState((prev) => ({ loginModalOpen: !prev.loginModalOpen }));
-  };
-
-  handleUserIconClick = () => {
-    const isLoggedIn = Boolean(this.props.auth?.id);
-    if (isLoggedIn) {
-      this.toggleUserMenu();
-    } else {
-      this.toggleLoginModal();
-    }
-  };
-
   render() {
-    const {
-      searchFocused,
-      searchText,
-      navbarScrolled,
-      isHomePage,
-      mobileNavOpen,
-      userMenuOpen,
-      loginModalOpen,
-    } = this.state;
-    const { auth, handleLogout } = this.props;
-    const isLoggedIn = Boolean(auth?.id);
-    const searchWidth = searchFocused ? "160px" : "30px";
+    const { navbarScrolled, isHomePage } = this.state;
     const navbarClass = `navbar ${navbarScrolled ? "scrolled" : ""} ${
       isHomePage ? "homepage" : ""
     }`;
-    const userMenuClass = `user-menu ${userMenuOpen ? "open" : ""}`;
 
     return (
       <div className={navbarClass}>
-        <span className="navbar-mobile">
-          <MenuIcon onClick={this.toggleMobileNav} />
-          <Modal
-            open={mobileNavOpen}
-            onClose={this.toggleMobileNav}
-            aria-labelledby="navigation"
-            aria-describedby="navigation"
-          >
-            <Box className="navbar-mobile-box" onClick={this.toggleMobileNav}>
-              <Link className="mobile-nav-link" to="/">
-                Home
-              </Link>
-              <Link className="mobile-nav-link" to="/candy">
-                Shop
-              </Link>
-              <Link className="mobile-nav-link" to="/about">
-                About
-              </Link>
-            </Box>
-          </Modal>
-        </span>
+        <MobileNavbar />
         <span className="navbar-left">
           <Link to="/">SS</Link>
           <Link to="/">Home</Link>
@@ -134,60 +53,10 @@ class Navbar extends Component {
           <Link to="/about">About</Link>
         </span>
         <span className="navbar-right">
-          <TextField
-            inputRef={(input) => {
-              this.searchInput = input;
-            }}
-            disabled={!searchFocused}
-            value={searchText}
-            onChange={this.handleSearchChange}
-            onBlur={this.handleFocusOut}
-            className="navbar-search"
-            InputProps={{
-              disableUnderline: !searchFocused,
-              startAdornment: (
-                <InputAdornment
-                  sx={{
-                    color: "#000",
-                  }}
-                  position="start"
-                  onClick={this.toggleSearch}
-                >
-                  <SearchIcon className="navbar-icon" />
-                </InputAdornment>
-              ),
-            }}
-            variant="standard"
-            style={{
-              width: searchWidth,
-              borderBottom: "none",
-              transition: "0.2s ease",
-            }}
-          />
-          <div className="user-menu-container">
-            <PersonIcon className="navbar-icon" onClick={this.handleUserIconClick} />
-            <div className={userMenuClass}>
-              <span>mnozawa@gmail.com</span>
-              <span>Account</span>
-              <span>Settings</span>
-              <span>Log Out</span>
-            </div>
-            <Modal open={loginModalOpen} onClose={this.toggleLoginModal}>
-              <div className="login-modal-style">asdfasdfasdfasdfasdf</div>
-            </Modal>
-          </div>
-          {isLoggedIn ? (
-            <span className="navbar-logout" onClick={() => handleLogout()}>
-              Log Out
-            </span>
-          ) : (
-            <>
-              <Link to="/login">Log In</Link>
-              <Link to="/signup">Sign Up</Link>
-            </>
-          )}
+          <NavbarSearch />
+          <AccountMenu />
           <Link className="icon-link" to="/cart">
-            <ShoppingCartIcon className="navbar-icon" />
+            <ShoppingCartIcon fontSize="large" className="navbar-icon" />
           </Link>
         </span>
       </div>
@@ -195,12 +64,8 @@ class Navbar extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-});
-
 const mapDispatchToProps = (dispatch) => ({
   handleLogout: () => dispatch(logout()),
 });
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Navbar));
+export default withRouter(connect(() => ({}), mapDispatchToProps)(Navbar));
