94e7a4d7ecf935c56edeb60e9b87dce982fcdf34
Zach Rizzo <RizzoZachary2@gmail.com>
2022-04-24 14:07:17 -0500

    admin components now display products and users

--- client/components/AdminPanel/ProductList.js
+++ client/components/AdminPanel/ProductList.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class ProductList extends Component{
+  constructor(){
+    super();
+    this.state = {
+      products: []
+    }
+  }
+
+  async componentDidMount(){
+    const products = (await axios.get('/api/candy')).data;
+    this.setState({
+      products
+    })
+  }
 
   render(){
+    const { products } = this.state
     return(
       <div>
         <h3>Products</h3>
+        <ul>
+          {products.map(product => {
+            return <li key={product.id}>{product.name}</li>
+          })}
+        </ul>
       </div>
     )
   }
--- client/components/AdminPanel/UserList.js
+++ client/components/AdminPanel/UserList.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 class UserList extends Component{
+  constructor(){
+    super();
+    this.state = {
+      users: []
+    }
+  }
+
+
+  async componentDidMount(){
+    const users = (await axios.get('/api/users')).data;
+    this.setState({
+      users
+    })
+  }
 
   render(){
+    const { users } = this.state
     return(
       <div>
         <h3>Users</h3>
+        <ul>
+          {users.map(user => {
+            return <li key={user.id}>{user.username}</li>
+          })}
+        </ul>
       </div>
     )
   }
