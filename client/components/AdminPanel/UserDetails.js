b1f748b5e9f4c8dcb285da35d74a0f7c30d9e48b
Zach Rizzo <RizzoZachary2@gmail.com>
2022-04-30 07:58:21 -0500

    added user details on admin panel and checkbox now displays correctly

--- client/components/AdminPanel/UserDetails.js
+++ client/components/AdminPanel/UserDetails.js
@@ -1,20 +1,44 @@
 import { Box, Modal } from "@mui/material";
 import React, { Component } from "react";
-
+import axios from "axios";
 
 class UserDetails extends Component {
-  
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      isAdmin: this.props.user.admin
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+
+  async doneUpdating(){
+    axios.put(`/api/users/${this.props.user.id}`)
+    this.props.done()
+  }
+
+  handleChange(evt){
+    this.setState({
+      isAdmin: !this.state.isAdmin
+    })
+    
+  }
 
   render(){
-    const { open, done, user } = this.props
+    const { isAdmin } = this.state;
+    const { open, done, user } = this.props;
     return(
-      <Modal open={open}>
+      <Modal open={open} >
         <Box sx={modalStyle}>
           <h3>{user.username}</h3>
           <h5>{user.email}</h5>
           <h5>{user.firstName}</h5>
           <h5>{user.lastName}</h5>
-          <label><input type="checkbox" id='admin' defaultChecked={user.admin}/>Admin</label>
+          <label><input type="checkbox" checked={isAdmin} onChange={this.handleChange}/>Admin</label>
           
           <button onClick={done}>Back</button>
         </Box>
