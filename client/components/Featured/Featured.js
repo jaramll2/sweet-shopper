b9aa5efc400b639d9c1e03dad41ace0aa6743b8b
Maya Lee <mayalee@MayasM1MacBook4.attlocal.net>
2022-04-30 09:06:12 -0700

    featuredItems will not repeat?

--- client/components/Featured/Featured.js
+++ client/components/Featured/Featured.js
@@ -9,7 +9,9 @@ class Featured extends React.Component {
     const { candies } = this.props
     const featured = []
     for (let i = 0; i < 3; i++) {
-      featured.push(candies[Math.floor(Math.random()*3)])
+      if (featured.indexOf(candies[Math.floor(Math.random()*10)]) === -1) {
+        featured.push(candies[Math.floor(Math.random()*10)])
+      }
     }
     if (featured.length === 0) {
       return <div>...Loading</div>
@@ -19,9 +21,9 @@ class Featured extends React.Component {
       <div className="featured">
         Featured Sweets
         <ul className="featured-items">
-          <li><Link to={`candy/id/${featured[0]?.id}`}>{featured[0]?.name}</Link></li>
-          <li><Link to={`candy/id/${featured[1]?.id}`}>{featured[1]?.name}</Link></li>
-          <li><Link to={`candy/id/${featured[0]?.id}`}>{featured[2]?.name}</Link></li>
+          <li><Link to={`candy/${featured[0]?.id}`}>{featured[0]?.name}</Link></li>
+          <li><Link to={`candy/${featured[1]?.id}`}>{featured[1]?.name}</Link></li>
+          <li><Link to={`candy/${featured[0]?.id}`}>{featured[2]?.name}</Link></li>
         </ul>
       </div>
     );
