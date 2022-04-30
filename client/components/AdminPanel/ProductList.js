1a6a1ab81c3d2f550d775722e5d7541234dd40de
Zach Rizzo <RizzoZachary2@gmail.com>
2022-04-30 09:22:26 -0500

    working on admin panel for the product list and fixed bug where seeding added newlines and spaces to candy names

--- client/components/AdminPanel/ProductList.js
+++ client/components/AdminPanel/ProductList.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import axios from "axios";
-
-import './AdminPanel.scss'
+import ProductDetails from './ProductDetails';
 
 class ProductList extends Component{
   constructor(){
     super();
     this.state = {
-      products: []
+      products: [],
+      displayDetails: false,
+      selectedProduct: {}
+
     }
+
+    this.closeProductInfo = this.closeProductInfo.bind(this)
   }
 
   async componentDidMount(){
@@ -18,15 +22,23 @@ class ProductList extends Component{
     })
   }
 
+  closeProductInfo(){
+    this.setState({
+      displayDetails: false
+    })
+  }
+
   render(){
-    const { products } = this.state
+    const { products, displayDetails, selectedProduct } = this.state
     return(
       <div className="admin-items">
+        {displayDetails ? <ProductDetails open={displayDetails} product={selectedProduct} done={this.closeProductInfo}/> : 
         <ul className="unordered-list">
           {products.map(product => {
-            return <li key={product.id} className="product">{product.name}</li>
+            return <li key={product.id} className="product" onClick={() => this.setState({displayDetails: true, selectedProduct: product})}>{product.name}</li>
           })}
         </ul>
+        }
       </div>
     )
   }
