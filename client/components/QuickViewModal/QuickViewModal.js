dc52f5be9dc41022d9e1b2601b11a0724a065383
Miyuki Nozawa <nozawa.myk@gmail.com>
2022-05-04 19:55:34 -0400

    add pagination style

--- client/components/QuickViewModal/QuickViewModal.js
+++ client/components/QuickViewModal/QuickViewModal.js
@@ -6,6 +6,7 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
 import { addToCart } from "../../store/cart";
+import { modalStyle } from "../../utils";
 
 import "./QuickViewModal.scss";
 
@@ -49,20 +50,6 @@ class QuickViewModal extends Component {
     )
   }
 }
-
-const modalStyle = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  background: "#ffffff",
-  transform: "translate(-50%, -50%)",
-  minWidth: "40%",
-  maxWidth: "80%",
-  border: "none",
-  outline: "none",
-  boxShadow: 24,
-  p: 4,
-};
 
 const mapStateToProps = state => state;
 const mapDispatchToProps = (dispatch) => {
